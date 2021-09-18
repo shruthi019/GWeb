378a0bf29725a39f265aad3f4383bc9f46c32b97
srishti-negi <srishtinegi249@gmail.com>
2021-09-18 17:14:31 +0530

    Integrated Google Speed Insight API

--- main_popup.js
+++ main_popup.js
@@ -1,5 +1,11 @@
 console.log("This is the js file for main popup")
 
+//########################################################\\
+//                                                        \\
+//             GREEN WEB FOUNDATION API                   \\
+//                                                        \\
+//########################################################\\
+
 const greenfoundation = 'https://api.thegreenwebfoundation.org/api/v3/greencheck/'
 
 const getHostname = (url) => {
@@ -37,3 +43,32 @@ async function update_status(){
     }
 
 document.getElementById("green_server").addEventListener("click", update_status);
+
+
+//########################################################\\
+//                                                        \\
+//           CHROME PAGE SPEED INSIGHTS API               \\
+//                                                        \\
+//########################################################\\
+
+const page_speed_api = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url="
+var r;
+
+function page_speed(){
+    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+    function(tabs){
+        var complete_url = page_speed_api + tabs[0].url
+        fetch(complete_url)
+        .then(response => response.json())  
+        .then(obj => {
+                r = obj
+                console.log(obj);
+                var result = obj.lighthouseResult.audits["total-byte-weight"].numericValue / 1000
+                document.getElementById("DATA_CONSUMED").innerHTML = result.toString() + " kb";
+            }
+            )    
+        .catch(err => console.log('Request Failed', err)); 
+    });
+}
+
+document.getElementById("green_server").addEventListener("click", page_speed);
